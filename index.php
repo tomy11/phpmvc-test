78c009e8a0f239977532bd0dc189c59dff642719
tomy11 <ohnemotive74@gmail.com>
2016-11-12 14:18:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require 'libs/Boots.php';
+require 'libs/Controller.php';
+require 'libs/Model.php';
+require 'libs/View.php';
+
+require 'config/database.php';
+require 'config/parth.php';
+$app = new boots();
